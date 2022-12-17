fd312414d162da37d53fd9ba7f96ed977b01e895
rrnax <agent47.rogal@gmail.com>
2022-12-17 03:52:10 +0100

    JS to Updates section and litlle corection

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+// Inicialization about updates section
+const meetings = document.querySelectorAll(".update-item");
+const details = document.querySelectorAll(".update-content");
+let eriler = meetings.length-1;
+details[meetings.length-1].style.display = 'block';
+
+// Updates section logic
+const btnPressed = e => {
+    let tmp = e.target.id;
+    console.log(tmp);
+    console.log(details[tmp]);
+    details[eriler].style.display = "none";
+    details[tmp].style.display = "block"
+    eriler = tmp;
+}
+
+for(let meat of meetings){
+    meat.addEventListener("click", btnPressed);
+}
+
 // Toggle button in navabar
 function toggleButton(){
     let minMenu = document.querySelector(".navamenu");
