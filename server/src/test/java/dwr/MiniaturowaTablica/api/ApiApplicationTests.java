4ee406ce024164a4953df3700e5d00d41245c9e5
Lachuuuu <108222128+Lachuuuu@users.noreply.github.com>
2023-04-17 17:21:04 +0200

    usuniecie testu

--- server/src/test/java/dwr/MiniaturowaTablica/api/ApiApplicationTests.java
+++ server/src/test/java/dwr/MiniaturowaTablica/api/ApiApplicationTests.java
@@ -1,13 +1,9 @@
 package dwr.MiniaturowaTablica.api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ApiApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
